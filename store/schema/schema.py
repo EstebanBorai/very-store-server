873e5af2686f1dad208afd1223d9668b35008355
Esteban Borai <estebanborai@gmail.com>
2021-12-03 20:04:24 -0300

    feat(gql): support `productsBySlug` query
    Implements support to query products by slug.

--- store/schema/schema.py
+++ store/schema/schema.py
@@ -16,11 +16,18 @@ class ProductObjectType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     products = graphene.List(ProductObjectType)
+    products_by_slug = graphene.List(
+        ProductObjectType, slug=graphene.String(required=True)
+    )
     categories = graphene.List(CategoryObjectType)
 
     @graphene.resolve_only_args
     def resolve_products(self):
         return Product.objects.all()
+
+    @graphene.resolve_only_args
+    def resolve_products_by_slug(self, slug):
+        return Product.objects.filter(slug=slug)
 
     @graphene.resolve_only_args
     def resolve_categories(self):
